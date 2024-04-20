4fe8254a5719313c27b84cd9d8ade9e17568b29e
Lee SangHyun <simon010809@gmail.com>
2024-04-20 21:36:45 +0900

    feat: BoardRepository, BoardService 개발
    BoardRepository - DB 작업 수행하기 위한 메서드 정의
    BoardService - Board 객체 받아서 db에 저장

---
+++ whats-in-my-ref/src/main/java/kr/ac/dankook/ace/whatsinmyref/service/BoardService.java
@@ -0,0 +1,20 @@
+package kr.ac.dankook.ace.whatsinmyref.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kr.ac.dankook.ace.whatsinmyref.entity.Board;
+import kr.ac.dankook.ace.whatsinmyref.repository.BoardRepository;
+
+@Service
+public class BoardService {
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+    public void write(Board board){
+
+        boardRepository.save(board);
+    }
+    
+}
