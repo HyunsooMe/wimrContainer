f382c02ce21b75873357c34e79f6a0667bd20fbd
hyunsooMe <khyunsooya@naver.com>
2024-05-20 17:41:22 +0900

    recipe entity 추가

---
+++ whats-in-my-ref/src/main/java/kr/ac/dankook/ace/whatsinmyref/controller/RecipeController.java
@@ -0,0 +1,11 @@
+package kr.ac.dankook.ace.whatsinmyref.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RecipeController {
+    @GetMapping("/api/load")
+    public ApiResponse
+
+}
