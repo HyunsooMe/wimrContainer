13e586a2eebaf5999f52edf03c6a31648f30ddb2
Lee SangHyun <simon010809@gmail.com>
2024-06-04 13:08:52 +0900

    머지 충동 해결

--- whats-in-my-ref/src/main/java/kr/ac/dankook/ace/whatsinmyref/repository/BoardRepository.java
+++ whats-in-my-ref/src/main/java/kr/ac/dankook/ace/whatsinmyref/repository/BoardRepository.java
@@ -9,9 +9,5 @@ import kr.ac.dankook.ace.whatsinmyref.entity.Board;
 
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Integer> {
-<<<<<<< HEAD
-    Page<Board> findAllByOrderByViewcountDesc(Pageable pageable);
-=======
   Page<Board> findAllByOrderByViewcountDesc(Pageable pageable);
->>>>>>> cfb302cc543b6baba4fb79e9229985fe2085fcc8
 } 
