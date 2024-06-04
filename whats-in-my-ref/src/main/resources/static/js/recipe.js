d50ab2ea04f8d8fb12fff9948053724d13969c64
D2Bro <32204045@dankook.ac.kr>
2024-06-04 21:04:46 +0900

    댓글 comment_check 수정 완료

--- whats-in-my-ref/src/main/resources/static/js/recipe.js
+++ whats-in-my-ref/src/main/resources/static/js/recipe.js
@@ -1,16 +1,18 @@
 //댓글의 입력 유무 체크
 function comment_check(){
     var form=document.getElementById("comment-form");
-    console.log(form.com.value);
-    if(!form.comment-content-input.value){
+   
+    if(form.commentContentInput.value.trim()===''){
+        alert("댓글 내용을 입력해주세요.")
         return false;
     }
     form.submit();
+    return true;
 }
 
 //글자 수 출력
 document.addEventListener('DOMContentLoaded', function () {
-    let textarea = document.getElementById('comment-content-input'),
+    let textarea = document.getElementById('commentContentInput'),
         charCount = document.getElementById('char-count');
 
     textarea.addEventListener('input', function () {
